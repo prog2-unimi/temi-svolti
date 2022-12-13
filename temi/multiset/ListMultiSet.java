d31e19141e664fe706d0d9c7519487e3805bd2c2
Massimo Santini <santini@di.unimi.it>
2022-12-13 11:46:58 +0100

    Updated to 2022

--- temi/multiset/ListMultiSet.java
+++ temi/multiset/ListMultiSet.java
@@ -1,3 +1,25 @@
+/*
+
+Copyright 2022 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -5,14 +27,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
-/**
- * Implementazione di {@link MultiSet} basata su una lista con ripetizioni.
- */
+/** Implementazione di {@link MultiSet} basata su una lista con ripetizioni. */
 public class ListMultiSet<E> extends AbstractMultiSet<E> {
 
-  /**
-   * La lista degli elementi del multiset.
-   */
+  /** La lista degli elementi del multiset. */
   private final List<E> elems = new LinkedList<>();
 
   /* L'invariante di rappresentazione è semplicemente dato dal fatto che la
@@ -30,7 +48,7 @@ public class ListMultiSet<E> extends AbstractMultiSet<E> {
 
   @Override
   public int remove(Object o) {
-    final int m = multiplicity(o); 
+    final int m = multiplicity(o);
     elems.remove(o);
     return m;
   }
@@ -47,12 +65,12 @@ public class ListMultiSet<E> extends AbstractMultiSet<E> {
 
   @Override
   public boolean contains(Object o) {
-    return elems.contains(o); 
+    return elems.contains(o);
   }
 
   @Override
   public int size() {
-    return elems.size(); 
+    return elems.size();
   }
 
   @Override
@@ -60,7 +78,7 @@ public class ListMultiSet<E> extends AbstractMultiSet<E> {
     Objects.requireNonNull(o);
     final ListMultiSet<E> result = new ListMultiSet<>();
     result.elems.addAll(elems);
-    for (final E elem: o) {
+    for (final E elem : o) {
       final int mult = o.multiplicity(elem) - multiplicity(elem);
       if (mult > 0) result.elems.addAll(Collections.nCopies(mult, elem));
     }
@@ -71,7 +89,7 @@ public class ListMultiSet<E> extends AbstractMultiSet<E> {
   public MultiSet<E> intersection(MultiSet<? extends E> o) {
     Objects.requireNonNull(o);
     final ListMultiSet<E> result = new ListMultiSet<>();
-    for (final E elem: this) {
+    for (final E elem : this) {
       final int mult = Math.min(multiplicity(elem), o.multiplicity(elem));
       if (mult > 0) result.elems.addAll(Collections.nCopies(mult, elem));
     }
@@ -81,11 +99,11 @@ public class ListMultiSet<E> extends AbstractMultiSet<E> {
   @Override
   public Iterator<E> iterator() {
     return new Iterator<>() {
-      
+
       private final Iterator<E> it = elems.iterator();
-      private E next = null; 
-      private int idx = -1; 
-      
+      private E next = null;
+      private int idx = -1;
+
       @Override
       public boolean hasNext() {
         if (next != null) return true;
@@ -107,7 +125,6 @@ public class ListMultiSet<E> extends AbstractMultiSet<E> {
         next = null;
         return result;
       }
-      
     };
   }
 
