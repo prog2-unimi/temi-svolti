b0a1f90eab6e4639171869d150c17293996f396c
Massimo Santini <santini@di.unimi.it>
2023-06-30 17:54:03 +0200

    New

---
+++ temi/cambiavalute/Cambi.java
@@ -0,0 +1,90 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+/** Classe che rappresenta l'elenco dei tassi noti al cambiavalute. */
+public class Cambi implements Iterable<Cambi.Tasso>{
+
+    /** Record che rappresenta una coppia di importi che descrivono un tasso di
+     * cambio. */
+    public record Tasso(Importo da, Importo a) {
+  
+    /**
+     * Crea un nuovo tasso di cambio.
+     *
+     * @param da l'importo di partenza.
+     * @param a l'importo di arrivo
+     * @throws NullPointerException se uno dei due importi è <code>null</code>.
+     * @throws IllegalArgumentException se uno dei due importi non è positivo o
+     *         se i due importi sono nella stessa valuta.
+     */
+    public Tasso(Importo da, Importo a) {
+      if (!Objects.requireNonNull(da, "Il primo importo non può essere null.").isPositive()) throw new IllegalArgumentException("Il primo importo deve essere positivo.");
+      if (!Objects.requireNonNull(a, "Il secodo importo non può essere null.").isPositive()) throw new IllegalArgumentException("Il secondo importo deve essere positivo.");
+      if (a.valuta.equals(da.valuta)) throw new IllegalArgumentException("Impossibile definire un tasso di cambio tra valute identiche");
+      this.da = da;
+      this.a = a;
+    }
+
+    @Override
+    public String toString() {
+      return da + " = " + a;
+     }
+  
+  }
+
+  /** L'elenco di cambi. Non deve essere o contenere <code>null</code> e non
+   * deve contenere più di un tasso tra due importi con una data coppia di
+   * valute. I tassi sono mantenuti in ordine di aggiunta/aggiornamento. */
+  private final List<Tasso> tassi = new LinkedList<>();
+
+  /** Cerca tra i tassi noti un tasso tra le valute date.
+   *
+   * @param da la valuta del primo importo.
+   * @param a la valuta del secondo importo.
+   * @return il tasso, oppure <code>null</code> se non è noto alcun tasso tra
+   * importi nelle valute date.
+   */
+  public Tasso cerca(Valuta da, Valuta a) {
+    for (Tasso t : tassi) 
+      if (t.da().valuta == da && t.a().valuta == a) 
+      return t;
+    return null;
+  }
+
+  /**
+   * Aggiorna, o aggiunge, il tasso di cambio tra due valute all'elenco dei
+   * tassi noti.
+   *
+   * <p>Nel caso fosse noto un tasso tra le due stesse valute del tasso da aggioranre, questo
+   * sostituirà il precedente tasso; viceversa il tasso da aggiornare è di fatto "nouovo" e verrà 
+   * semplicemente aggiunto alla lista dei tassi noti.
+   *
+   *
+   * @param tasso il tasso da aggiornare.
+   * @return <code>true</code> se il tasso sostituisce un tasso precedentemente noto,
+   *         <code>false</code> viceversa.
+   */
+  public boolean aggiorna(Tasso tasso) {
+    Tasso precedente = cerca(tasso.da().valuta, tasso.a().valuta);
+    if (precedente != null) tassi.remove(precedente);
+    tassi.add(tasso);
+    return precedente != null;
+  }
+
+  @Override
+  public Iterator<Tasso> iterator() {
+    return Collections.unmodifiableList(tassi).iterator();
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer sb = new StringBuffer("Tassi:\n");
+    Iterator<Tasso> it = tassi.iterator();
+    while (it.hasNext())
+      sb.append(it.next() + (it.hasNext() ? "\n" : ""));
+    return sb.toString();
+  }
+}
---
+++ temi/cambiavalute/Cassa.java
@@ -0,0 +1,74 @@
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+
+/** Classe che implementa una cassa multi-valuta.
+ *
+ * La cassa consente di iterare sui propri importi diversi da zero.
+ */
+public class Cassa implements Iterable<Importo> {
+  
+  /** Mappa che associa a ciascuna valuta il suo importo in cassa, valute e
+   * importi non devono essere <code>null</code>, gli importi devono essere
+   * positivi e avere valuta corrispondente alla chiave. */
+  private final Map<Valuta, Importo> valuta2importo = new EnumMap<>(Valuta.class);
+  
+  /**
+   * Restituisce l'importo totale presente in cassa in una data valuta.
+   *
+   * @param valuta la valuta.
+   * @return l'importo.
+   * @throws NullPointerException se la valuta è <code>null</code>.
+   */
+  public Importo totale(Valuta valuta) {
+    Importo totale = valuta2importo.get(Objects.requireNonNull(valuta, "La valuta non può essere null."));
+    return totale == null ? Importo.zero(valuta) : totale;
+  }
+
+  /**
+   * Versa un importo in cassa.
+   * 
+   * @param importo l'importo da versare.
+   * @throws NullPointerException se l'importo è <code>null</code>.
+   * @throws IllegalArgumentException se l'importo è negativo.
+   */
+  public void versa(Importo importo) {
+    if (!Objects.requireNonNull(importo, "L'importo non può essere null.").isZero()) return;
+    if (!importo.isPositive()) throw new IllegalArgumentException("Non si possono depositare importi negativi.");
+    valuta2importo.put(importo.valuta, totale(importo.valuta).somma(importo));
+  }
+
+  /**
+   * Preleva un importo dalla cassa.
+   *
+   * @param importo l'importo da prelevare.
+   * @throws NullPointerException se l'importo è <code>null</code>.
+   * @throws IllegalArgumentException se l'importo è negativo, o superiore al
+   * totale in cassa per la sua valuta.
+   */
+  public void preleva(Importo importo) {
+    if (!Objects.requireNonNull(importo, "L'importo non può essere null.").isZero()) return;
+    if (!importo.isPositive()) throw new IllegalArgumentException("Non si possono depositare importi negativi.");
+    final Importo resto = totale(importo.valuta).differenza(importo);
+    if (resto.isZero()) valuta2importo.remove(resto.valuta);
+    else if (!resto.isPositive()) throw new IllegalArgumentException("La cassa non contiene abbastanza fondi.");
+    else valuta2importo.put(importo.valuta, resto);
+  }
+
+  @Override
+  public Iterator<Importo> iterator() {
+    return Collections.unmodifiableCollection(valuta2importo.values()).iterator();
+  }
+  
+  @Override
+  public String toString() {
+    StringBuffer sb = new StringBuffer("Cassa:\n");
+    Iterator<Importo> it = iterator();
+    while (it.hasNext())
+      sb.append(it.next() + (it.hasNext() ? "\n" : ""));      
+    return sb.toString();
+  }
+
+}
